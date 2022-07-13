6304dcd4638e67ee277d30331a407fcef32b425e
Shurygina_Yulia <zavalishina-yu@mail.ru>
2022-07-14 00:00:07 +0300

    parsing fixed

--- parsing.java
+++ parsing.java
@@ -1,45 +1,61 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
+
 
 public class parsing {
-    public static HashMap<Integer, String> digitParsing(char [] array, int i) {
-        HashMap<Integer, String> indexAndString = new HashMap<>();
+    public static String [] digitParsing(char [] array, int i) {
+        String [] indexAndString = new String[2];
         String result = "";
         while (Character.isDigit(array[i])){
             result += array[i];
             i++;
         }
-        indexAndString.put(i, result);
+        indexAndString[0] = Integer.toString(i);
+        indexAndString[1] = result;
         return indexAndString;
         }
-    public static HashMap<Integer, String> digitParsing(char [] array, int i) {
-        HashMap<Integer, String> indexAndString = new HashMap<>();
+    public static String [] letterParsing(char [] array, int i) {
+        String [] indexAndString = new String[2];
         String result = "";
-        while (Character.isDigit(array[i])){
+        while (Character.isLetter(array[i])){
             result += array[i];
             i++;
         }
-        indexAndString.put(i, result);
+        indexAndString[0] = Integer.toString(i);
+        indexAndString[1] = result;
         return indexAndString;
         }
         
     
     public static void main(String [] args) {
-        String exp = "(2^3*(10/(5-3)))";
+        String exp = "(2^3 * (10 / (5 - 3)))^(Sin(Pi))";
         char [] array = exp.toCharArray();
-        int i = 0;
+        List<String> list = new LinkedList<>();
+        int i = 0; 
         while (i < array.length){
             if (Character.isDigit(array[i])){
-                HashMap<Integer, String> indexAndString = digitParsing(array, i);
-                
-                i = indexAndString.get(0);
-                String str = indexAndString.getValue(i);
+                String [] indexAndString = digitParsing(array, i);
+                i = Integer.parseInt(indexAndString[0]);
+                String str = indexAndString[1];
+                list.add(str);
             }
-            if (Character.isLetter(array[i])){
-
+            else if (Character.isLetter(array[i])){
+                String [] indexAndString = letterParsing(array, i);
+                i = Integer.parseInt(indexAndString[0]);
+                String str = indexAndString[1];
+                list.add(str);   
             }
-    }
+            else if (array[i] == ' '){
+                i++;
+                continue;
+            } else {
+               list.add(Character.toString(array[i]));
+                i++;
+            }
+        }
+        String[] parsedArray = list.toArray(new String[list.size()]); 
+        for (int j = 0; j < parsedArray.length; j++){
+            System.out.println(parsedArray[j]);
+        }
     
 }
 }
